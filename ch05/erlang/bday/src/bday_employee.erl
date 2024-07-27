1536b5688d08d7a2f057b5bf7ef12e194a45f097
Meraj Molla <meraj.enigma@gmail.com>
2024-07-27 15:28:09 -0400

    employee module

---
+++ ch05/erlang/bday/src/bday_employee.erl
@@ -0,0 +1,23 @@
+-module(bday_employee).
+-export([from_csv/1]).
+
+-ifdef(TEST).
+-export([adapt_csv_result/1]).
+-endif.
+
+-opaque employee() :: #{string() :=  term()}.
+-opaque handle() :: {raw, [employee()]}.
+-export_type([handle/0, employee/0]).
+
+-spec from_csv(string()) -> handle().
+from_csv(String) ->
+  {raw, [adapt_csv_result(Map) || Map <- bday_csv:decode(String)]}.
+
+-spec adapt_csv_result(map()) -> employee().
+adapt_csv_result(Map) ->
+  maps:fold(fun(K,V, NewMap) -> NewMap#{trim(K) => maybe_null(trim(V))} end, #{}, Map).
+
+trim(Str) -> string:trim(Str, leading, " ").
+
+maybe_null("") -> undefined;
+maybe_null(Str) -> Str.
---
+++ ch05/erlang/bday/test/prop_bday_employee.erl
@@ -0,0 +1,27 @@
+-module(prop_bday_employee).
+-include_lib("proper/include/proper.hrl").
+
+%% Properties
+prop_fix_csv_leading_space() ->
+  ?FORALL(Map, raw_employee_map(),
+    begin
+      Emp = bday_employee:adapt_csv_result(Map),
+      Strs = [X || X <- maps:keys(Emp) ++ maps:values(Emp), is_list(X)],
+      lists:all(fun(String) -> hd(String) =/= $\s end, Strs)
+    end).
+
+%% Generators
+raw_employee_map() ->
+  ?LET(PropList,
+      [{"last_name", prop_csv:field()},
+        {" first_name", whitespaced_text()},
+        {" date_of_birth", text_date()},
+        {" email", whitespaced_text()}],
+    maps:from_list(PropList)).
+
+whitespaced_text() ->
+  ?LET(Txt, prop_csv:field(), " " ++ Txt).
+
+text_date() ->
+  ?LET({Y,M,D}, {choose(1900,2020), choose(1,12), choose(1,31)},
+    lists:flatten(io_lib:format(" ~w/~2..0w/~2..0w", [Y,M,D]))).
