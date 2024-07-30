e082de66df9f103d01fd70a3b46c053a209a9149
Meraj Molla <meraj.enigma@gmail.com>
2024-07-30 07:02:45 -0400

    checkout without specials (erlang)

---
+++ ch06/erlang/pbt/src/checkout.erl
@@ -0,0 +1,9 @@
+-module(checkout).
+-export([total/3]).
+
+-type item() :: string().
+-type price() :: integer().
+
+-spec total([item()], [{item(), price()}], any()) -> price().
+total(ItemList, PriceList, _Specials) ->
+  lists:sum([proplists:get_value(Item, PriceList) || Item <- ItemList]).
---
+++ ch06/erlang/pbt/test/prop_checkout.erl
@@ -0,0 +1,27 @@
+-module(prop_checkout).
+-include_lib("proper/include/proper.hrl").
+
+%% Properties
+prop_no_special1() ->
+  ?FORALL({ItemList, ExpectedPrice, PriceList}, item_price_list(),
+    ExpectedPrice =:= checkout:total(ItemList, PriceList, [])).
+
+%% Generators
+item_price_list() ->
+  ?LET(PriceList, price_list(),
+      ?LET({ItemList, ExpectedPrice}, item_list(PriceList),
+        {ItemList, ExpectedPrice, PriceList})).
+
+price_list() ->
+  ?LET(PriceList, non_empty(list({non_empty(string()), integer()})),
+    lists:ukeysort(1, PriceList)).
+
+item_list(PriceList) ->
+  ?SIZED(Size, item_list(Size, PriceList, {[], 0})).
+
+item_list(0, _PriceList, Acc) -> Acc;
+item_list(N, PriceList, {ItemAcc, ExpectedPrice}) ->
+    ?LET({Item, Price}, elements(PriceList),
+      item_list(N-1, PriceList, {[Item|ItemAcc], Price + ExpectedPrice})).
+
+
