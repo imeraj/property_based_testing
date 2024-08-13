2835301c515aae0045aad57411e76f20a9df875e
Meraj Molla <meraj.enigma@gmail.com>
2024-08-13 06:45:43 -0400

    targeted properties trees (erlang)

--- ch06/erlang/pbt/test/prop_target.erl
+++ ch06/erlang/pbt/test/prop_target.erl
@@ -2,6 +2,49 @@
 -include_lib("proper/include/proper.hrl").
 -compile(export_all).
 
+%% Properties
+prop_tree_regular(opts) -> [{numtests, 1000}].
+prop_tree_regular() ->
+  ?FORALL(T, tree(),
+    begin
+      Weight = sides(T),
+      io:format(" ~p", [Weight]),
+      true
+    end).
+
+prop_tree() ->
+  ?FORALL_TARGETED(T, tree(),
+    begin
+      {Left, Right} = Weight = sides(T),
+      io:format(" ~p", [Weight]),
+      ?MAXIMIZE(Left-Right),
+      true
+    end).
+
+%% Generators
+tree() ->
+  ?LET(L, non_empty(list(integer())), to_tree(L)).
+
+to_tree(L) ->
+  lists:foldl(fun insert/2, undefined, L).
+
+insert(N, {node, N, L, R}) -> {node, N, L, R};
+insert(N, {node, M, L, R}) when N < M -> {node, M, insert(N, L), R};
+insert(N, {node, M, L, R}) when N > M -> {node, M, L, insert(N, R)};
+insert(N, {leaf, N}) -> {leaf, N};
+insert(N, {leaf, M}) when N < M -> {node, N, undefined, {leaf, M}};
+insert(N, {leaf, M}) when N > M -> {node, N, {leaf, M}, undefined};
+insert(N, undefined) -> {leaf, N}.
+
+sides({node, _, Left, Right}) ->
+  {LL, LR} = sides(Left),
+  {RL, RR} = sides(Right),
+  {count_inner(Left) + LL + LR, count_inner(Right) + RL + RR};
+sides(_) -> {0,0}.
+
+count_inner({node, _, _, _}) -> 1;
+count_inner(_) -> 0.
+
 prop_path(opts) -> [{search_steps, 100}].
 prop_path() ->
   ?FORALL_TARGETED(P, path(),
